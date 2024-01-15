0a13fa7b5f4d50bdfbf321e64d4fc4e6b7807828
Albina <11albina19@gmail.com>
2024-01-15 20:16:46 +0400

    fix: static pages redesigned

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get 'others/about'
-  get '/about', to: 'others#about'
-  get '/about/author', to: 'others#author'
+
 end
--- test/controllers/others_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class OthersControllerTest < ActionDispatch::IntegrationTest
-  test "should get about" do
-    get others_about_url
-    assert_response :success
-  end
-end
