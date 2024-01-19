cc09d9d8a81285d100a4cfd4ea85b8480889eb33
Albina <11albina19@gmail.com>
2024-01-19 16:38:14 +0400

    fix: user attributes changed

--- db/migrate/20240118231157_create_users.rb
+++ db/migrate/20240118231157_create_users.rb
@@ -1,13 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :login, null: false
       t.string :password, null: false
-      t.string :mail
-      t.string :role, null: false
-      t.string :tests_history
-      t.datetime :last_visit_date
-      t.datetime :registration_date
+      t.string :mail, null: false
 
       t.timestamps
     end
