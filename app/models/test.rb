916d397a142215fc863bf5ed43d587ea05592667
Albina <11albina19@gmail.com>
2024-03-28 20:11:21 +0400

    feat: added sort_name class method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,10 @@
 class Test < ApplicationRecord
+
+  def self.sort_name (category)
+    array = Test.where(category_id: category.id).pluck(:title)
+    array.sort! {|a,b| b <=> a}
+    array
+  end
+
+
 end
