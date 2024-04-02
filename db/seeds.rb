f5cc5c2c0882fd95689aa7d03d3dc5a8e1b56c25
Albina <11albina19@gmail.com>
2024-04-02 14:28:54 +0400

    refactor: the create method has been replaced by the create! method. empty line added

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,26 +7,26 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-category_db = Category.create(title: "Базы данных")
-category_g = Category.create(title: "Git")
-category_ror = Category.create(title: "Ruby on Rails")
-category_os = Category.create(title: "OS")
+category_db = Category.create!(title: "Базы данных")
+category_g = Category.create!(title: "Git")
+category_ror = Category.create!(title: "Ruby on Rails")
+category_os = Category.create!(title: "OS")
 
-user1 = User.create(password: '12345', mail: 'ivan@google.com')
-user2 = User.create(password: 'qwerty', mail: 'petr@mail.ru',)
-admin = User.create(password: 'pU7pol6s', mail: '11albina19@gmail.com')
+user1 = User.create!(password: '12345', mail: 'ivan@google.com')
+user2 = User.create!(password: 'qwerty', mail: 'petr@mail.ru',)
+admin = User.create!(password: 'pU7pol6s', mail: '11albina19@gmail.com')
 
-test1 = Test.create(title: "Тест на основы баз данных", level: 1, category_id: category_db.id, author_id: admin.id)
-test2 = Test.create(title: "Тест на оптимизацию запросов", level: 3, category_id: category_db.id, author_id: admin.id)
-test3 = Test.create(title: "Тест на знание linux", level: 2, category_id: category_os.id, author_id: admin.id)
-test4 = Test.create(title: "Тест на основы git", level: 1, category_id: category_g.id, author_id: admin.id)
-test5 = Test.create(title: "Тест на знание миграций в Rails", level: 2, category_id: category_ror.id, author_id: admin.id)
+test1 = Test.create!(title: "Тест на основы баз данных", level: 1, category_id: category_db.id, author_id: admin.id)
+test2 = Test.create!(title: "Тест на оптимизацию запросов", level: 3, category_id: category_db.id, author_id: admin.id)
+test3 = Test.create!(title: "Тест на знание linux", level: 2, category_id: category_os.id, author_id: admin.id)
+test4 = Test.create!(title: "Тест на основы git", level: 1, category_id: category_g.id, author_id: admin.id)
+test5 = Test.create!(title: "Тест на знание миграций в Rails", level: 2, category_id: category_ror.id, author_id: admin.id)
 
-question1 = Question.create(body: 'Понятие функций и триггеров', test_id: test1.id)
-question2 = Question.create(body: 'В чем заключается третья нормальная форма?', test_id: test1.id)
-question3 = Question.create(body: 'Чем жесткая ссылка отличается от символической?', test_id: test3.id)
-question4 = Question.create(body: 'Что такое ветка?', test_id: test4.id)
-question5 = Question.create(body: 'Что такое миграция?', test_id: test5.id)
+question1 = Question.create!(body: 'Понятие функций и триггеров', test_id: test1.id)
+question2 = Question.create!(body: 'В чем заключается третья нормальная форма?', test_id: test1.id)
+question3 = Question.create!(body: 'Чем жесткая ссылка отличается от символической?', test_id: test3.id)
+question4 = Question.create!(body: 'Что такое ветка?', test_id: test4.id)
+question5 = Question.create!(body: 'Что такое миграция?', test_id: test5.id)
 
 answers_data = [
   { body: 'Небольшой боковой отросток, побег дерева, кустарника или травянистого растения', correct: false, question_id: question4.id },
@@ -38,9 +38,9 @@ answers_data = [
 ]
 
 answers_data.each do |answer_data|
-  Answer.create(answer_data)
+  Answer.create!(answer_data)
 end
 
-Result.create(user_id: admin.id, test_id: test1.id)
-Result.create(user_id: user1.id, test_id: test2.id)
-Result.create(user_id: user1.id, test_id: test4.id)
+Result.create!(user_id: admin.id, test_id: test1.id)
+Result.create!(user_id: user1.id, test_id: test2.id)
+Result.create!(user_id: user1.id, test_id: test4.id)
