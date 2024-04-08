cee024278641584ccbe19576c530783f3d6ea3a5
Albina <11albina19@gmail.com>
2024-04-08 18:09:37 +0400

    refactor: fixed query in model Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
 
   def self.sort_tests_selection_category(category_name)
-    Category.where(title: category_name).joins(:tests).order('tests.title DESC').pluck('tests.title')
+    Test.joins("INNER JOIN categories ON tests.category_id = categories.id").where(categories: { title: category_name }).order('tests.title DESC').pluck('tests.title')
   end
 end
