8e759e554f36fbcf26a6b72d634a4164cce2ccc3
Albina <11albina19@gmail.com>
2024-04-08 16:07:39 +0400

    fix: fixed query in model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 
   def user_tests_at_level(level)
-    Test.where(id: Result.where(user_id: self.id).select(:test_id)).where(level: level)
+    #Test.where(id: Result.where(user_id: self.id).select(:test_id)).where(level: level)
+    Test.where(level: level).joins("INNER JOIN results ON tests.id = results.test_id").where(results: { user_id: self.id })
   end
 end
