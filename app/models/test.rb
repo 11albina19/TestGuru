77f8c825b54f10e131c60c13885e49a893bfdb35
Albina <11albina19@gmail.com>
2024-04-08 22:48:19 +0400

    refactor: code made more readable

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,9 @@
 class Test < ApplicationRecord
 
   def self.sort_tests_selection_category(category_name)
-    Test.joins("INNER JOIN categories ON tests.category_id = categories.id").where(categories: { title: category_name }).order('tests.title DESC').pluck('tests.title')
+    Test.joins("INNER JOIN categories ON tests.category_id = categories.id")
+        .where(categories: { title: category_name })
+        .order('tests.title DESC')
+        .pluck('tests.title')
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 
   def user_tests_at_level(level)
-    Test.where(level: level).joins("INNER JOIN results ON tests.id = results.test_id").where(results: { user_id: self.id })
+    Test.where(level: level).joins("INNER JOIN results ON tests.id = results.test_id")
+        .where(results: { user_id: self.id })
   end
 end
