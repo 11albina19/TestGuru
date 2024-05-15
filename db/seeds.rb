f890ed1a673a06522b4b99a4976de994b86609c7
Albina <11albina19@gmail.com>
2024-05-15 19:52:40 +0400

    refactor: seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,42 +7,50 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-category_db = Category.create!(title: "Базы данных")
-category_g = Category.create!(title: "Git")
-category_ror = Category.create!(title: "Ruby on Rails")
-category_os = Category.create!(title: "OS")
+categories = Category.create!([
+  { title: "Базы данных" },
+  { title: "Git" },
+  { title: "Ruby on Rails" },
+  { title: "OS" }
+])
 
-user1 = User.create!(password: '12345', mail: 'ivan@google.com')
-user2 = User.create!(password: 'qwerty', mail: 'petr@mail.ru',)
-admin = User.create!(password: 'pU7pol6s', mail: '11albina19@gmail.com')
+users = User.create!([
+  { password: 'pU7pol6s', mail: '11albina19@gmail.com' },
+  { password: '12345', mail: 'ivan@google.com' },
+  { password: 'qwerty', mail: 'petr@mail.ru' }
+])
 
-test1 = Test.create!(title: "Тест на основы баз данных", level: 1, category_id: category_db.id, author_id: admin.id)
-test2 = Test.create!(title: "Тест на оптимизацию запросов", level: 3, category_id: category_db.id, author_id: admin.id)
-test3 = Test.create!(title: "Тест на знание linux", level: 2, category_id: category_os.id, author_id: admin.id)
-test4 = Test.create!(title: "Тест на основы git", level: 1, category_id: category_g.id, author_id: admin.id)
-test5 = Test.create!(title: "Тест на знание миграций в Rails", level: 2, category_id: category_ror.id, author_id: admin.id)
+tests = Test.create!([
+  { title: "Тест на основы баз данных", level: 1, category: categories[0], author: users[2] },
+  { title: "Тест на оптимизацию запросов", level: 3, category: categories[0], author: users[2] },
+  { title: "Тест на знание linux", level: 2, category: categories[3], author: users[2] },
+  { title: "Тест на основы git", level: 1, category: categories[1], author: users[2] },
+  { title: "Тест на знание миграций в Rails", level: 2, category: categories[2], author: users[2] }
+])
 
-question1 = Question.create!(body: 'Понятие функций и триггеров', test_id: test1.id)
-question2 = Question.create!(body: 'В чем заключается третья нормальная форма?', test_id: test1.id)
-question3 = Question.create!(body: 'Чем жесткая ссылка отличается от символической?', test_id: test3.id)
-question4 = Question.create!(body: 'Что такое ветка?', test_id: test4.id)
-question5 = Question.create!(body: 'Что такое миграция?', test_id: test5.id)
+questions = Question.create!([
+  { body: 'Понятие функций и триггеров', test: tests[0] },
+  { body: 'В чем заключается третья нормальная форма?', test: tests[0] },
+  { body: 'Чем жесткая ссылка отличается от символической?', test: tests[2] },
+  { body: 'Что такое ветка?', test: tests[3] },
+  { body: 'Что такое миграция?', test: tests[4]}
+])
 
 answers_data = [
-  { body: 'Небольшой боковой отросток, побег дерева, кустарника или травянистого растения', correct: false, question_id: question4.id },
-  { body: 'Отдельная линия в системе дорог, отклоняющаяся в сторону от основного пути.', correct: false, question_id: question4.id },
-  { body: 'Набор коммитов, расположенных в хронологическом порядке', correct: true, question_id: question4.id },
-  { body: 'Миграции - механизм ActiveRecord, который позволяет вносить изменения в структуру базы данных: создавать и удалять таблицы, переименовывать, добавлять и удалять поля.', correct: true, question_id: question5.id },
-  { body: 'Миграции -  это способ упростить большие контроллеры или модели для лучшего понимания и управления.', correct: false, question_id: question5.id },
-  { body: 'Миграции - это переработка кода, процесс изменения внутренней структуры программы, в ходе которой происходит перенос кода из одних классов в другие', correct: false, question_id: question5.id },
+  { body: 'Небольшой боковой отросток, побег дерева, кустарника или травянистого растения', correct: false, question: questions[3] },
+  { body: 'Отдельная линия в системе дорог, отклоняющаяся в сторону от основного пути.', correct: false, question: questions[3] },
+  { body: 'Набор коммитов, расположенных в хронологическом порядке', correct: true, question: questions[3] },
+  { body: 'Миграции - механизм ActiveRecord, который позволяет вносить изменения в структуру базы данных: создавать и удалять таблицы, переименовывать, добавлять и удалять поля.', correct: true, question: questions[4] },
+  { body: 'Миграции -  это способ упростить большие контроллеры или модели для лучшего понимания и управления.', correct: false, question: questions[4] },
+  { body: 'Миграции - это переработка кода, процесс изменения внутренней структуры программы, в ходе которой происходит перенос кода из одних классов в другие', correct: false, question: questions[4] }
 ]
 
 Answer.create!(answers_data)
 
 result_data = [
-  {user_id: admin.id, test_id: test1.id},
-  {user_id: user1.id, test_id: test2.id},
-  {user_id: user1.id, test_id: test4.id}
+  { user: users[0], test: tests[0] },
+  { user: users[1], test: tests[1] },
+  { user: users[1], test: tests[3] }
 ]
 
 Result.create!(result_data)
