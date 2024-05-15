cb7564e22d33383945611971afe05996d7c20dee
Albina <11albina19@gmail.com>
2024-05-15 17:42:20 +0400

    fix: fixed associations in test and user

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,7 +3,8 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: 'User'
 
   has_many :questions, dependent: :destroy
-  has_many :users, class_name: 'Result', foreign_key: :test_id
+  has_many :results, foreign_key: :test_id
+  has_many :users, through: :results
 
   def self.sort_tests_selection_category(category_name)
     joins("INNER JOIN categories ON tests.category_id = categories.id")
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_many :created_tests, class_name: 'Test', foreign_key: :author_id
-  has_many :passed_tests, class_name: 'Result', foreign_key: :user_id
-  
+  has_many :results, foreign_key: :user_id
+  has_many :passed_tests, through: :results, source: :test
+
   def user_tests_at_level(level)
     Test.where(level: level).joins("INNER JOIN results ON tests.id = results.test_id")
         .where(results: { user_id: self.id })
