691e9f7188ccc6be414fa8f16fb7a59ac82f45dd
Albina <11albina19@gmail.com>
2024-06-20 23:08:17 +0400

    feat: add scope

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,6 +6,10 @@ class Test < ApplicationRecord
   has_many :results, dependent: :destroy
   has_many :users, through: :results
 
+  scope :easy, -> { where(level: [0..1]) }
+  scope :medium, -> { where(level: [2..4]) }
+  scope :hard, -> { where(level: [5..Float::INFINITY]) }
+
   def self.sort_tests_selection_category(category_name)
     joins(:category)
       .where(categories: { title: category_name })
