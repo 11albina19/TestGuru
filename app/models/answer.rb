c6c76658497e78821a7a4ab71a5754aa21bd91ca
Albina <11albina19@gmail.com>
2024-06-24 23:28:09 +0400

    validation fixed

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,13 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
+  validate :validate_quantity_answers, on: :create
 
   scope :correct, -> { where(correct: true) }
+
+  private
+
+  def validate_quantity_answers
+    errors.add(:answers, "quantity of answers from 1 to 4") if answers.count >= 4
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -6,12 +6,4 @@ class Question < ApplicationRecord
   validate :validate_quantity_answers
 
   has_many :answers, dependent: :destroy
-
-  private
-
-  def validate_quantity_answers
-    if answers.size < 1 || answers.size > 4
-      errors.add(:answers, "quantity of answers from 1 to 4")
-    end
-  end
 end
