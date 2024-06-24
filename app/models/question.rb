9d43de55d3fb84c0ea029d7e1fd0b41bc60ddac0
Albina <11albina19@gmail.com>
2024-06-24 23:51:28 +0400

    refactor: code style errors fixed

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,8 @@
 class Question < ApplicationRecord
   belongs_to :test
 
-  validates :body, presence: true
-
-  validate :validate_quantity_answers
-
   has_many :answers, dependent: :destroy
+
+  validates :body, presence: true
+  validate :validate_quantity_answers
 end
