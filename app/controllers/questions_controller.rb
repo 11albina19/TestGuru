cb2e5598c34bb47d68e1a45f80f9cd36d22c1c2d
Albina <11albina19@gmail.com>
2024-09-18 16:59:40 +0400

    fix: display of question has been fixed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -16,7 +16,7 @@ class QuestionsController < ApplicationController
   def create
     @question = @test.questions.new(question_params)
     if @question.save
-      redirect_to test_path(@question.test)
+      redirect_to @question
     else
       render :new
     end
