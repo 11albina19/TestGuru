06cf0db3f404777128ad49efa2618671ebcba6ef
Albina <11albina19@gmail.com>
2024-10-16 23:33:39 +0400

    feat: template modified

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -1,2 +1,22 @@
 module ResultsHelper
+
+  def success_rate(result)
+    100 / result.test.questions.count * result.correct_question
+  end
+
+  def return_color(result)
+    if success_rate(result) >= 85
+      'text-success'
+    else
+      'text-fail'
+    end
+  end
+
+  def return_message(result)
+    if success_rate(result) >= 85
+      'Тест успешно пройден'
+    else
+      'Тест не пройден'
+    end
+  end
 end
