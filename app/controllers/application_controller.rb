c953844114379cecea617bbbaf4c7dd954bc4b2d
Albina <11albina19@gmail.com>
2025-01-17 18:10:55 +0400

    feat: video 1 completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+  before_action :set_locale
+
+  def default_url_options
+    { lang: I18n.locale }
+  end
 
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || (resource.admin? ?  admins_tests_path : super)
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
