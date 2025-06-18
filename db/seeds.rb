332850bf62c0463c01d9db10e402a1129a9aa82b
albina <11albina19@gmail.com>
2025-06-18 23:31:20 +0400

    add  gist delete_all

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+Gist.delete_all
 Result.delete_all
 Answer.delete_all
 Question.delete_all
