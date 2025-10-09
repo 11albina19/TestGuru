cae1064972ae701371b22e4d04352da6ab01feb8
albina <11albina19@gmail.com>
2025-10-09 22:27:02 +0400

    in progress badge_get_service

--- app/services/badge_get_service.rb
+++ app/services/badge_get_service.rb
@@ -2,7 +2,7 @@ class BadgeGetService
 
   RULES = {
     category: ::Badges::CategoryRule,
-    first_try: ::Badges::CategoryRule,
+    first_try: ::Badges::FirstTryRule,
     level: ::Badges::CategoryRule
   }.freeze
 
@@ -25,19 +25,18 @@ class BadgeGetService
   @user.badges << badge_in_category if (tests_in_category - passed_tests_in_category).empty?
   
   #ПРАВИЛО 2
-  #первая попытка: выбрать все бейджи, где правило - first_try, тут параметр видимо пустой
-  badge_first_try = Badge.where(rule_type: :first_try)
-  #!!!!!!!ЕСЛИ ТАКОЙ БЭЙДЖ ЕСТЬ!!!!!! тут нужно сначала удостовериться, что тест пройден с 1 попытки
-  #стоп, еще надо убедиться, что не просто пройден, а правильно пройден
+  #поправить
   
-  @user.badges << badge_first_try if Result.where(user_id: @user, test_id: @result.test).count == 1
-    
   #ПРАВИЛО 3
   #уровень: выбрать все бейджи, где правило - level, И уровень в параметре совпадает с уровнем теста
   badge_in_level = Badge.where(rule_type: :level, parameter: @result.test.level)
 
   @user.badges << badge_in_level if badge_in_level.any?
  
+  RULES.each do |key, value|
+    value.check_the_rule(key, @result)
+  end
+
   binding.pry 
   end
 end
---
+++ app/services/badges/first_try_rule.rb
@@ -0,0 +1,12 @@
+module Badges
+  class FirstTryRule
+    class << self
+      def check_the_rule(rule_name, @result)
+        badge_first_try = Badge.where(rule_type: :first_try)
+        #if @result.successful? && badge_first_try.exist?
+      @user.badges << badge_first_try if Result.where(user_id: @user, test_id: @result.test).count == 1
+        
+      end
+    end
+  end
+end
