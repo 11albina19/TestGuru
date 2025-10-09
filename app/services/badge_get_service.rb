d117487c9085d1005a7ab83cc406e0500e1569b0
albina <11albina19@gmail.com>
2025-10-09 20:47:50 +0400

    in progress...

--- app/services/badge_get_service.rb
+++ app/services/badge_get_service.rb
@@ -12,7 +12,7 @@ class BadgeGetService
   end
 
   def call
-  badge_array = []
+  #ПРАВИЛО 1
   #категория: 1. выбрать 1 бейдж, где правило - category И категория 
   #в параметре совпадает с категорией пройденного теста
   #получим 1 бейдж или несколько? по идее, 1, несколько таких нет смысла создавать
@@ -20,13 +20,24 @@ class BadgeGetService
 
   #!!!!!!!ЕСЛИ ТАКОЙ БЭЙДЖ ЕСТЬ!!!!!! проверка, прошел ли пользователь ВСЕ тесты в этой категории
   tests_in_category = Test.where(category_id: @result.test.category)
-  passed_tests = Result.where(user_id: @user, test_id: tests_in_category)
-  passed_all = (tests_in_category - passed_tests).empty?
-  badge_array << badge_in_category if passed_all
-  #первая попытка: выбрать все бейджи, где правило - first_try, тут параметр видимо пустой
-  #но тут нужно сначала удостовериться, что тест пройден с 1 попытки
+  passed_tests_in_category = Result.where(user_id: @user, test_id: tests_in_category)
+ 
+  @user.badges << badge_in_category if (tests_in_category - passed_tests_in_category).empty?
   
+  #ПРАВИЛО 2
+  #первая попытка: выбрать все бейджи, где правило - first_try, тут параметр видимо пустой
+  badge_first_try = Badge.where(rule_type: :first_try)
+  #!!!!!!!ЕСЛИ ТАКОЙ БЭЙДЖ ЕСТЬ!!!!!! тут нужно сначала удостовериться, что тест пройден с 1 попытки
+  #стоп, еще надо убедиться, что не просто пройден, а правильно пройден
+  
+  @user.badges << badge_first_try if Result.where(user_id: @user, test_id: @result.test).count == 1
+    
+  #ПРАВИЛО 3
   #уровень: выбрать все бейджи, где правило - level, И уровень в параметре совпадает с уровнем теста
+  badge_in_level = Badge.where(rule_type: :level, parameter: @result.test.level)
+
+  @user.badges << badge_in_level if badge_in_level.any?
+ 
   binding.pry 
   end
 end
