542fe453b70c15d08e2a35ae4e90e4dd8389c358
albina <11albina19@gmail.com>
2025-10-10 12:48:13 +0400

    feat: category_rule and level_rule done

--- app/services/badges/category_rule.rb
+++ app/services/badges/category_rule.rb
@@ -5,10 +5,19 @@ module Badges
         user = result.user
         category_title = result.test.category.title
         
-        badge_in_category = Badge.where(rule_type: rule_name, parameter: category_title)
+        badge_in_category = Badge
+          .where(rule_type: rule_name, parameter: category_title)
+
         if badge_in_category.exists? && result.successful?
-          tests_in_category = Test.where(category_id: result.test.category).pluck(:id)
-          passed_tests_in_category = Result.where(user_id: user, test_id: tests_in_category).pluck(:test_id).uniq
+          tests_in_category = Test
+            .where(category_id: result.test.category)
+            .pluck(:id)
+
+          passed_tests_in_category = Result
+            .where(user_id: user, test_id: tests_in_category)
+            .select(&:successful?)
+            .map(&:test_id)
+            .uniq
  
           user.badges << badge_in_category if (tests_in_category - passed_tests_in_category).empty?
         end
