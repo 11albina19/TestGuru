a65217740e9f209032542ac27a7e770f088ecff5
albina <11albina19@gmail.com>
2025-10-12 15:49:18 +0400

    feat: таймер работает

--- app/javascript/controllers/timer_controller.js
+++ app/javascript/controllers/timer_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static values = { finishAt: Number, serverTime: Number}
+  static values = { finishAt: Number, serverTime: Number, redirectUrl: String}
   static targets = ["output"]
 
   connect() {
@@ -10,17 +10,21 @@ export default class extends Controller {
     this.timer = setInterval(() => this.tick(), 1000)
   }
 
+  disconnect() {
+    clearInterval(this.timer)
+  }
+
   tick() {
     const now = Math.floor(Date.now() / 1000) + this.offset
     const secondsLeft = Math.floor(this.finishAtValue - now)
 
     if (secondsLeft <= 0) {
-      //а тут переадресовать на результат
+      clearInterval(this.timer)
+      Turbo.visit(this.redirectUrlValue)
     } else {
       const minutes = Math.floor(secondsLeft / 60)
       const seconds = secondsLeft % 60
       this.outputTarget.textContent = `${minutes}:${seconds.toString().padStart(2, "0")}`
-      debugger
     }
   }
 }
