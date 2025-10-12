7f184de5e9ffb627ca03a155f66cad51bf134351
albina <11albina19@gmail.com>
2025-10-12 15:13:56 +0400

    in progress: вроде все нормально, но таймер не выводится

--- app/javascript/controllers/timer_controller.js
+++ app/javascript/controllers/timer_controller.js
@@ -1,7 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static values = { finishAt: Number, serverTime: Number}
+  static targets = ["output"]
+
   connect() {
-    this.element.textContent = "Hello World!"
+    this.offset = this.serverTimeValue - Math.floor(Date.now() / 1000)
+    this.tick()
+    this.timer = setInterval(() => this.tick(), 1000)
+  }
+
+  tick() {
+    const now = Math.floor(Date.now() / 1000) + this.offset
+    const secondsLeft = Math.floor(this.finishAtValue - now)
+
+    if (secondsLeft <= 0) {
+      //а тут переадресовать на результат
+    } else {
+      const minutes = Math.floor(secondsLeft / 60)
+      const seconds = secondsLeft % 60
+      this.outputTarget.textContent = `${minutes}:${seconds.toString().padStart(2, "0")}`
+      debugger
+    }
   }
 }
